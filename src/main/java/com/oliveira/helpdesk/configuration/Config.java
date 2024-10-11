d22ccbb732f8c5d28edb2c1a10d05eb1b3a4b626
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2024-10-11 13:34:42 -0300

    Refactor user configuration bd

--- src/main/java/com/oliveira/helpdesk/configuration/Config.java
+++ src/main/java/com/oliveira/helpdesk/configuration/Config.java
@@ -21,12 +21,9 @@ public class Config {
 
   public void veryfyAdminBd() {
 
+    // CONFIG ABOUT ADMINISTRATOR
     var userAdmin = this.repository.findByUsername("admin");
-    if (userAdmin.isPresent()) {
-      System.out.println("------------------------- Administrador already saved in BD -------------------------");
-
-    } else {
-
+    if (!userAdmin.isPresent()) {
       UserEntity entity = new UserEntity();
       entity.setCreatedAt(new Date());
       entity.setUpdateAt(new Date());
@@ -38,9 +35,35 @@ public class Config {
       entity.setUsername("admin");
 
       this.repository.save(entity);
-      var adminUser = this.repository.findByUsername("admin");
-      System.out.println("------------------------- Administrador CREATE AND SAVE in BD ------------------------- "
-          + adminUser.isPresent());
+      userAdmin = this.repository.findByUsername("admin");
+      System.out.println("------------------------- ADMINISTRATOR CREATE AND SAVE IN BD ------------------------- "
+          + userAdmin.isPresent());
+
+    } else {
+      System.out.println("------------------------- ADMINISTRATOR ALREADY EXIST IN BD -------------------------");
+
+    }
+
+    // CONFIG ABOUT ADMINISTRATOR
+    var userSuport = this.repository.findByUsername("suport");
+    if (!userSuport.isPresent()) {
+      UserEntity entity = new UserEntity();
+      entity.setUpdateAt(new Date());
+      entity.setCreatedAt(new Date());
+      entity.setPassword(new BCryptPasswordEncoder().encode("password"));
+      entity.setActive(true);
+      entity.setRole(UserRole.SUPPORT_ATTENDANT);
+      entity.setEmail("suport@");
+      entity.setName("Suport Attendent");
+      entity.setUsername("suport");
+
+      this.repository.save(entity);
+      userSuport = this.repository.findByUsername("admin");
+      System.out.println("------------------------- SUPORT USER CREATE AND SAVE IN BD ------------------------- "
+          + userSuport.isPresent());
+
+    } else {
+      System.out.println("------------------------- SUPORT USER ALREADY EXIST IN BD -------------------------");
 
     }
 
